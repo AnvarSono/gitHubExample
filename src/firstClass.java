1e5c499acc57906ae994f58b2e9ea445320d91fb
AnvarSono <56326018+AnvarSono@users.noreply.github.com>
2019-11-09 15:16:48 -0600

    Update firstClass.java

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,16 +1,5 @@
 public class firstClass {
     public static void main(String[] args) {
-        System.out.println("first class");
-        System.out.println("business class");
-        if(true){
-            System.out.println("hello");
-
-
-        }
-        for(;;){
-            System.out.println("first");
-            System.out.println("second");
-        }
-
+  
     }
 }
