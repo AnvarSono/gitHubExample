0b0d62fa319610f7b4b00887100676af42d0c189
Anvar Sono <kerimbekovich89@gmail.com>
2019-11-09 13:02:29 -0600

    edit

--- src/firstClass.java
+++ src/firstClass.java
@@ -2,6 +2,7 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("first class");
         System.out.println("business class");
-       
+        System.out.println("tko");
+
     }
 }
